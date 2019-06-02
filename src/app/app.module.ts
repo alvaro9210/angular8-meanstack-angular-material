5d0a83f95d6ff1d2f53baf3884c87afb4cc8dc9d
Alvaro Contreras <alvaro9210@gmail.com>
2019-06-01 22:24:10 -0700

    Add StudentsList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddStudentComponent } from './components/add-student/add-student.component';
 import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { StudentsListComponent } from './components/students-list/students-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddStudentComponent,
-    EditStudentComponent
+    EditStudentComponent,
+    StudentsListComponent
   ],
   imports: [
     BrowserModule,
